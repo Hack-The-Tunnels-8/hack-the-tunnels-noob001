dfd1664555809ddbb6426c97a8ae2727442b78eb
Rahat <rahat3062@gmail.com>
2023-09-30 12:16:01 -0400

    Hello world!

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -22,6 +22,7 @@ function Home() {
         <h1 className="home-page__title">Home</h1>
         <h2>Products:</h2>
         <div className="home-page__products">
+          <p>Hello World</p>
           {products.map((product) => (
             <Link to={`/products/${product.id}`} key={`${product.id}`}>
               <ProductPreviewCard
